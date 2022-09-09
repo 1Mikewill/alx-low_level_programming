fbd54351b4fdda4bad9d904bcc6c8cf184b5d22d
1Mikewill <mikewilliam459@gmail.com>
2022-09-09 08:13:23 -0700

    committed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
  *
  * * main - Printing
  *
- * 
+ * *
  *
  * * Return: Always 0 (Successful)
  *
@@ -16,7 +16,9 @@ int main(void)
 
 {
 
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual
+
+			puzzle");
 
 	return (0);
 
