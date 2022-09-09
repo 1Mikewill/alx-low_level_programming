14505d776e04973527a21aed3a7556494bfe9dc2
1Mikewill <mikewilliam459@gmail.com>
2022-09-09 07:49:19 -0700

    c programme

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,22 +6,17 @@
  *
  * *
  *
- * * Return: 0
+ * * Return: 0(Success)
  *
  * */
 
 int main(void)
 
 {
-
 	char a;
-
 	int b;
-
 	long int c;
-
 	long long int d;
-
 	float e;
 
 	printf("Size of a char: %d byte(s)\n", sizeof(a));
