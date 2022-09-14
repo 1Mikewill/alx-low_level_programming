931806093cb01188637b99765a6c8f134384194d
1Mikewill <mikewilliam459@gmail.com>
2022-09-14 03:13:21 -0700

    betty

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,8 +2,8 @@
 
 /**
  *_islower - function that checks for lowercase character
- *@c:  is the int that will use for the argument of the function
- *return: 0 
+ *@c: is the int that will use for the argument of the function
+ *return: returns 1 or 0 depending on condition
  */
 
 int _islower(int c)
