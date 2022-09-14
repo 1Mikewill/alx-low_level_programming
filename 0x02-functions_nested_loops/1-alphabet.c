94e69c012088aa511fd7052ef05fda0180de3270
1Mikewill <mikewilliam459@gmail.com>
2022-09-14 01:40:52 -0700

    df

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *main - prints out lowercase alphabet
+ *return= always alphabet
+ */
+
+void print_alphabet(void);
+{
+	int b;
+
+	for (j = 'a' ; j <= 'z' ; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+	return (0);
+}
