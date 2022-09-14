75dc1eef098e09d7a46ff8e8f227fd6ceec3f3ab
1Mikewill <mikewilliam459@gmail.com>
2022-09-14 04:00:50 -0700

    fifth

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *print_sign - check for a sign
+ *@n: character for the argument
+ *Return: 1 0 and -1 depending on condition
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return ('-1');
+	}
+	else
+	{
+		_putchar('0');
+		return ('0');
+	}
+}
