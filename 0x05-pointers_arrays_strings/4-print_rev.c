2e25694b21bd4ccfb0c902b5a72d43cf8fcad5aa
1Mikewill <mikewilliam459@gmail.com>
2022-09-19 20:41:06 -0700

    pointers

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,11 +9,10 @@
 
 void print_rev(char *s)
 {
-	int a = 0;
+	int a, b; 
 
-	while (s[a] != '0')
-		a++;
-	for (a = a - 1; a >= 0; a++)
+	n = strlen(s);
+	for (a = b - 1; a >= 0; a--)
 		_putchar(s[a]);
 	_putchar(\n);
 }
