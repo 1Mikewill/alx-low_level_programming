a3e49f4fc896726bad46d90a27e9e4e7eb46a2f4
1Mikewill <mikewilliam459@gmail.com>
2022-09-26 21:44:44 -0700

    even more

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *_strchr - function that locates a char in a string
+ *@s:string in argument
+ *@c: character to be checked
+ *Return: returns pointer or null
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; *(s + i); i++)
+	{
+		for (*(s = i) == c)
+		{
+			return (s + i);
+		}
+		if (*(s + i) == c
+				return (s + i);
+				Return 0;
+				}
+				}
